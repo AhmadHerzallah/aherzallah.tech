ffddbf7b0b187ab3e640c7edc9fa47eb2a25f9fd
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2022-04-01 18:54:20 +0300

    .

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,7 +20,7 @@ import {
 } from "../../styles/GlobalComponents";
 import { projects } from "../../constants/constants";
 
-const prefix = "aherzallah.tech";
+const prefix = "aherzallah.tech/";
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
